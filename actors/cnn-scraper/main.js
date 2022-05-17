debef7f963c27e20a519c1c99050829dc74ec826
Equidem <matsochor@seznam.cz>
2022-05-17 19:09:11 +0200

    Commiting CNN Scraper

---
+++ actors/cnn-scraper/main.js
@@ -0,0 +1,10 @@
+import Apify from 'apify';
+
+Apify.main(async () => {
+    const input = await Apify.getInput();
+
+    await Apify.metamorph('lukaskrivka/article-extractor-smart', {
+        ...input,
+        ...JSON.parse("{\"startUrls\":[{\"url\":\"https://edition.cnn.com\"}],\"onlyNewArticles\":false,\"onlyNewArticlesPerDomain\":false,\"onlyInsideArticles\":true,\"enqueueFromArticles\":false,\"scanSitemaps\":false,\"useGoogleBotHeaders\":false,\"mustHaveDate\":true,\"isUrlArticleDefinition\":{\"minDashes\":4,\"hasDate\":true,\"linkIncludes\":[\"article\",\"storyid\",\"?p=\",\"id=\",\"/fpss/track\",\".html\",\"/content/\"]},\"proxyConfiguration\":{\"useApifyProxy\":true},\"useBrowser\":true,\"extendOutputFunction\":\"($) => {\\n    const result = {};\\n    // Uncomment to add a title to the output\\n    // result.pageTitle = $('title').text().trim();\\n\\n    return result;\\n}\",\"minWords\":100,\"pageWaitMs\":5,\"maxArticlesPerCrawl\":1000,\"linkSelector\":\"a[href]\",\"pseudoUrls\":[{\"url\":\"https://edition.cnn.com[.*]\"}]}".replace(/\\"/g,"\"")),
+    });
+});
