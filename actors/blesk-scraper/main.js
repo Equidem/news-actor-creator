52911c539a9c14b7b6fed0e3051283fb6fc81e8e
Equidem <matsochor@seznam.cz>
2022-08-03 14:41:30 +0200

    ..

--- actors/blesk-scraper/main.js
+++ actors/blesk-scraper/main.js
@@ -6,13 +6,13 @@ Apify.main(async () => {
     console.log(input);
     console.log(JSON.parse("{\"startUrls\":[{\"url\":\"https://www.blesk.cz/\"}],\"onlyNewArticles\":false,\"onlyNewArticlesPerDomain\":false,\"onlyInsideArticles\":true,\"enqueueFromArticles\":true,\"scanSitemaps\":false,\"useGoogleBotHeaders\":false,\"mustHaveDate\":true,\"isUrlArticleDefinition\":{\"linkIncludes\":[\"clanek\",\".html\"]},\"proxyConfiguration\":{\"useApifyProxy\":true},\"useBrowser\":false,\"extendOutputFunction\":\"($) => {\\n    const result = {};\\n    // Uncomment to add a title to the output\\n    // result.pageTitle = $('title').text().trim();\\n\\n    return result;\\n}\",\"maxArticlesPerCrawl\":1000,\"minWords\":0}".replace(/\\"/g,"\"")));
 
-    console.log({
+    const metamorphInput = {
         ...input,
         ...JSON.parse("{\"startUrls\":[{\"url\":\"https://www.blesk.cz/\"}],\"onlyNewArticles\":false,\"onlyNewArticlesPerDomain\":false,\"onlyInsideArticles\":true,\"enqueueFromArticles\":true,\"scanSitemaps\":false,\"useGoogleBotHeaders\":false,\"mustHaveDate\":true,\"isUrlArticleDefinition\":{\"linkIncludes\":[\"clanek\",\".html\"]},\"proxyConfiguration\":{\"useApifyProxy\":true},\"useBrowser\":false,\"extendOutputFunction\":\"($) => {\\n    const result = {};\\n    // Uncomment to add a title to the output\\n    // result.pageTitle = $('title').text().trim();\\n\\n    return result;\\n}\",\"maxArticlesPerCrawl\":1000,\"minWords\":0}".replace(/\\"/g,"\"")),
-    });
+    }
 
-    await Apify.metamorph('lukaskrivka/article-extractor-smart', {
-        ...input,
-        ...JSON.parse("{\"startUrls\":[{\"url\":\"https://www.blesk.cz/\"}],\"onlyNewArticles\":false,\"onlyNewArticlesPerDomain\":false,\"onlyInsideArticles\":true,\"enqueueFromArticles\":true,\"scanSitemaps\":false,\"useGoogleBotHeaders\":false,\"mustHaveDate\":true,\"isUrlArticleDefinition\":{\"linkIncludes\":[\"clanek\",\".html\"]},\"proxyConfiguration\":{\"useApifyProxy\":true},\"useBrowser\":false,\"extendOutputFunction\":\"($) => {\\n    const result = {};\\n    // Uncomment to add a title to the output\\n    // result.pageTitle = $('title').text().trim();\\n\\n    return result;\\n}\",\"maxArticlesPerCrawl\":1000,\"minWords\":0}".replace(/\\"/g,"\"")),
-    });
+    console.log(metamorphInput);
+
+    await Apify.metamorph('lukaskrivka/article-extractor-smart', metamorphInput);
+
 });
