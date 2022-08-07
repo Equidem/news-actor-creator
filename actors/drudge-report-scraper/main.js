5d0619062a56ca3e1492e79e8f8a507d241c0f74
Equidem <matsochor@seznam.cz>
2022-08-07 20:43:58 +0200

    Commiting Drudge Report Scraper

--- actors/drudge-report-scraper/main.js
+++ actors/drudge-report-scraper/main.js
@@ -4,7 +4,7 @@ Apify.main(async () => {
     const input = await Apify.getInput();
 
     await Apify.metamorph('lukaskrivka/article-extractor-smart', {
+        ...JSON.parse("{\"startUrls\":[{\"url\":\"https://www.drudgereport.com\"}],\"onlyNewArticles\":false,\"onlyNewArticlesPerDomain\":false,\"onlyInsideArticles\":false,\"enqueueFromArticles\":false,\"scanSitemaps\":false,\"useGoogleBotHeaders\":false,\"mustHaveDate\":true,\"isUrlArticleDefinition\":{\"minDashes\":4,\"hasDate\":true,\"linkIncludes\":[\"article\",\"storyid\",\"?p=\",\"id=\",\"/fpss/track\",\".html\",\"/content/\",\"news/\"]},\"proxyConfiguration\":{\"useApifyProxy\":true},\"useBrowser\":false,\"extendOutputFunction\":\"($) => {\\n    const result = {};\\n    // Uncomment to add a title to the output\\n    // result.pageTitle = $('title').text().trim();\\n\\n    return result;\\n}\",\"minWords\":150}".replace(/\\"/g,"\"").replace(/__ESCAPED_QUOTES__/g,"\\\"")),
         ...input,
-        ...JSON.parse("{\"startUrls\":[{\"url\":\"https://www.drudgereport.com\"}],\"onlyNewArticles\":false,\"onlyNewArticlesPerDomain\":false,\"onlyInsideArticles\":false,\"enqueueFromArticles\":false,\"scanSitemaps\":false,\"useGoogleBotHeaders\":false,\"mustHaveDate\":true,\"isUrlArticleDefinition\":{\"minDashes\":4,\"hasDate\":true,\"linkIncludes\":[\"article\",\"storyid\",\"?p=\",\"id=\",\"/fpss/track\",\".html\",\"/content/\",\"news/\"]},\"proxyConfiguration\":{\"useApifyProxy\":true},\"useBrowser\":false,\"extendOutputFunction\":\"($) => {\\n    const result = {};\\n    // Uncomment to add a title to the output\\n    // result.pageTitle = $('title').text().trim();\\n\\n    return result;\\n}\",\"minWords\":150}".replace(/\\"/g,"\"")),
     });
 });
